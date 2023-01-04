6ee00bca1cf757c0fcda417d39d4d459a13fad78
Denis Listiadi <denislistiadi24@gmail.com>
2023-01-04 11:13:44 +0700

    Updates | Filter and Sorting Products

--- controllers/productController.js
+++ controllers/productController.js
@@ -15,11 +15,36 @@ const createProduct = asyncHandler(async (req, res) => {
   }
 })
 
-// Get All Product
+// Get All Product and Filter Product
 const getAllProduct = asyncHandler(async (req, res) => {
   try {
-    const products = await Product.find()
-    res.json({ products })
+    // Filtering
+    let queryObj = { ...req.query }
+    const excludeFields = ["page", "sort", "limit", "fields"]
+    excludeFields.forEach((el) => delete queryObj[el])
+
+    let queryStr = JSON.stringify(queryObj)
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => "$" + match)
+
+    let query = Product.find(JSON.parse(queryStr))
+
+    // Sorting
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(",").join(" ")
+      query = query.sort(sortBy)
+    } else {
+      query = query.sort("-createdAt")
+    }
+
+    // limiting fields
+    if (req.query.fields) {
+      const fields = req.query.fields.split(",").join(" ")
+      query = query.select(fields)
+    } else {
+      query = query.select("-__v")
+    }
+    const products = await query
+    res.json(products)
   } catch (error) {
     throw new Error(error)
   }
